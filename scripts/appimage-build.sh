c4cab59bd2b55ef9d0192e5911811442d34d0e14
Masterlocker <wsurew@gmail.com>
2026-09-08 13:15:29 +0200

    Add libiec61883.so.0

--- scripts/appimage-build.sh
+++ scripts/appimage-build.sh
@@ -112,6 +112,7 @@ else
   cp /usr/lib/x86_64-linux-gnu/libcdio.so.19 Supersonic.AppDir/usr/lib/ #void linux + CachyOS fix 
   cp /usr/lib/x86_64-linux-gnu/libraw1394.so.11 Supersonic.AppDir/usr/lib/ #void linux fix
   cp /usr/lib/x86_64-linux-gnu/librom1394.so.0 Supersonic.AppDir/usr/lib/ #requirement for libraw1394
+  cp /usr/lib/x86_64-linux-gnu/libiec61883.so.0 Supersonic.AppDir/usr/lib/ #requirement for libraw1394
   set +x
 fi
 
